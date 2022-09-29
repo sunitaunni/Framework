a030a8a457621b53903cc3d73a23e8e5b7849ad4
sunitaunni <unni.sunita@gmail.com>
2022-09-29 23:14:00 +0530

    comment removed

--- Framework/src/test/java/com/learnautomation/testcases/LoginTestCRM.java
+++ Framework/src/test/java/com/learnautomation/testcases/LoginTestCRM.java
@@ -11,11 +11,9 @@ public class LoginTestCRM extends BaseClass {
 	@Test
 	public void loginApp()
 	{				
-		//FIRST TESTCASE
 		LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
 		loginpage.loginToCRM(excel.getStringData("Login", 0, 0), excel.getStringData("Login", 0, 1));
 		Helper.captureScreenshot(driver);
-		
 	}
 
 }
